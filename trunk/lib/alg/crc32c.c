0e4ded69a41405fe60c7bf04f07c5e58277a2ca5
cperciva <cperciva@158ec33e-9530-43ce-08a2-6ca7ccd1c3f5>
2011-09-10 04:51:58 +0000

    MFlibcperciva r9 (put T arrays into BSS instead of using malloc).

--- trunk/lib/alg/crc32c.c
+++ trunk/lib/alg/crc32c.c
@@ -11,13 +11,13 @@
  * T[2][i] = reverse32(reverse8(i) * x^48 mod p(x) mod 2)
  * T[3][i] = reverse32(reverse8(i) * x^56 mod p(x) mod 2)
  */
-static int initdone = 0;
-static uint32_t * T[4];
+static uint32_t T0[256];
+static uint32_t T1[256];
+static uint32_t T2[256];
+static uint32_t T3[256];
 
 /* Optimization: Precomputed value of T[0][0x80]. */
 #define T_0_0x80 0x82f63b78
-
-static void done(void);
 
 /**
  * reverse(x):
@@ -36,105 +36,62 @@ reverse(uint32_t x)
 	return (x);
 }
 
-/**
- * done(void):
- * Clean up tables.
- */
-static void
-done(void)
+static uint32_t
+times256(uint32_t r)
 {
+	int k;
 
-	free(T[3]);
-	free(T[2]);
-	free(T[1]);
-	free(T[0]);
+	/* Shift left one bit at a time. */
+	for (k = 0; k < 8; k++) {
+		if (r & 0x80000000)
+			r = (r << 1) ^ 0x1EDC6F41;
+		else
+			r = (r << 1);
+	}
+
+	return (r);
 }
 
 /**
  * init(void):
  * Initialize tables.
  */
-static int
+static void
 init(void)
 {
-	size_t i, j, k;
+	size_t i;
 	uint32_t r;
-
-	/* Are we already initialized? */
-	if (initdone)
-		return (0);
-
-	/* Allocate space for arrays. */
-	if ((T[0] = malloc(256 * sizeof(uint32_t))) == NULL)
-		goto err0;
-	if ((T[1] = malloc(256 * sizeof(uint32_t))) == NULL)
-		goto err1;
-	if ((T[2] = malloc(256 * sizeof(uint32_t))) == NULL)
-		goto err2;
-	if ((T[3] = malloc(256 * sizeof(uint32_t))) == NULL)
-		goto err3;
 
 	/* Fill in tables. */
 	for (i = 0; i < 256; i++) {
 		r = reverse(i);
-		for (j = 0; j < 4; j++) {
-			for (k = 0; k < 8; k++) {
-				if (r & 0x80000000)
-					r = (r << 1) ^ 0x1EDC6F41;
-				else
-					r = (r << 1);
-			}
-			T[j][i] = reverse(r);
-		}
+		T0[i] = reverse(r = times256(r));
+		T1[i] = reverse(r = times256(r));
+		T2[i] = reverse(r = times256(r));
+		T3[i] = reverse(r = times256(r));
 	}
 
-	/*
-	 * Clean up the tables when we exit, in order to simplify the task
-	 * of tracking down memory leaks.
-	 */
-	if (atexit(done))
-		goto err4;
-
 	/* Make sure we optimized correctly. */
-	assert(T[0][0x80] == T_0_0x80);
-
-	/* Success! */
-	initdone = 1;
-	return (0);
-
-err4:
-	free(T[3]);
-err3:
-	free(T[2]);
-err2:
-	free(T[1]);
-err1:
-	free(T[0]);
-err0:
-	/* Failure! */
-	return (-1);
+	assert(T0[0x80] == T_0_0x80);
 }
 
 /**
  * CRC32C_Init(ctx):
- * Initialize a CRC32C-computing context.  This function can only fail the
- * first time it is called.
+ * Initialize a CRC32C-computing context.
  */
-int
+void
 CRC32C_Init(CRC32C_CTX * ctx)
 {
+	static int initdone = 0;
 
 	/* Initialize tables. */
 	if (initdone == 0) {
-		if (init())
-			return (-1);
+		init();
+		initdone = 1;
 	}
 
 	/* Set state to the CRC of the implicit leading 1 bit. */
 	ctx->state = T_0_0x80;
-
-	/* Success! */
-	return (0);
 }
 
 /**
@@ -149,16 +106,16 @@ CRC32C_Update(CRC32C_CTX * ctx, const uint8_t * buf, size_t len)
 	/* Handle blocks of 4 bytes. */
 	for (; len >= 4; len -= 4, buf += 4) {
 		ctx->state =
-		    T[0][((ctx->state >> 24) & 0xff) ^ buf[3]] ^
-		    T[1][((ctx->state >> 16) & 0xff) ^ buf[2]] ^
-		    T[2][((ctx->state >> 8)  & 0xff) ^ buf[1]] ^
-		    T[3][((ctx->state)       & 0xff) ^ buf[0]];
+		    T0[((ctx->state >> 24) & 0xff) ^ buf[3]] ^
+		    T1[((ctx->state >> 16) & 0xff) ^ buf[2]] ^
+		    T2[((ctx->state >> 8)  & 0xff) ^ buf[1]] ^
+		    T3[((ctx->state)       & 0xff) ^ buf[0]];
 	}
 
 	/* Handle individual bytes. */
 	for (; len > 0; len--, buf++) {
 		ctx->state = (ctx->state >> 8) ^
-		    T[0][((ctx->state) & 0xff) ^ buf[0]];
+		    T0[((ctx->state) & 0xff) ^ buf[0]];
 	}
 }
 

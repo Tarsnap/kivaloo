de29422770ee913c0c677e32c5bd590f47a453df
Colin Percival <cperciva@tarsnap.com>
2011-10-10 18:45:30 +0000

    Reworking of packet read path, step 2: Add wire_readpacket_{peek, wait, wait_cancel, consume} functions for accessing a packet directly out of the internal read buffer rather than copying it and invoking a callback.

--- lib/wire/wire_readpacket.c
+++ lib/wire/wire_readpacket.c
@@ -1,9 +1,11 @@
+#include <assert.h>
 #include <errno.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "crc32c.h"
+#include "events.h"
 #include "mpool.h"
 #include "netbuf.h"
 #include "sysendian.h"
@@ -11,40 +13,220 @@
 
 #include "wire.h"
 
-struct readpacket {
-	int (*callback)(void *, struct wire_packet *);
+struct read_cookie {
+	int (* callback)(void *, struct wire_packet *);
 	void * cookie;
 	struct netbuf_read * R;
 	struct wire_packet * packet;
-	uint8_t hbuf[16];
+	void * wait_cookie;
 };
 
-MPOOL(readpacket, struct readpacket, 16);
+struct wait_cookie {
+	struct netbuf_read * R;
+	int (* callback)(void *, int);
+	void * cookie;
+};
 
-static int docallback(struct readpacket *, struct wire_packet *);
-static int readheader(void *, int);
-static int readrec(void *, int);
+MPOOL(read_cookie, struct read_cookie, 16);
+MPOOL(wait_cookie, struct wait_cookie, 16);
 
-/* Do the callback and free the cookie. */
-static int
-docallback(struct readpacket * RP, struct wire_packet * packet)
+static int callback_wait_gotheader(void *, int);
+static int callback_wait_gotdata(void *, int);
+static int callback_read_gotpacket(void *, int);
+
+/**
+ * wire_readpacket_peek(R, P):
+ * Look to see if a packet is available from the buffered reader ${R}.  If
+ * yes, store it in the packet structure ${P}; otherwise, set ${P}->buf to
+ * NULL.  On error (including if a corrupt packet is received) return -1.
+ */
+int
+wire_readpacket_peek(struct netbuf_read * R, struct wire_packet * P)
 {
-	int rc;
+	CRC32C_CTX ctx;
+	uint8_t * data;
+	size_t datalen;
+	uint8_t cbuf[4];
+	size_t i;
 
-	/* Invoke the callback. */
-	rc = (RP->callback)(RP->cookie, packet);
+	/* No packet data yet. */
+	P->buf = NULL;
 
-	/* If we didn't pass the packet upstream, free it. */
-	if (packet == NULL) {
-		free(RP->packet->buf);
-		wire_packet_free(RP->packet);
+	/* Ask the buffered reader what it has. */
+	netbuf_read_peek(R, &data, &datalen);
+
+	/* If we have less than 20 bytes, we don't have a complete packet. */
+	if (datalen < 20)
+		goto nopacket;
+
+	/* Verify the header checksum. */
+	CRC32C_Init(&ctx);
+	CRC32C_Update(&ctx, data, 12);
+	CRC32C_Final(cbuf, &ctx);
+	if (memcmp(&data[12], cbuf, 4)) {
+		warn0("Incorrect CRC on packet header");
+		goto failed;
 	}
 
-	/* Free the cookie. */
-	mpool_readpacket_free(RP);
+	/* Parse ID and length. */
+	P->ID = be64dec(&data[0]);
+	P->len = be32dec(&data[8]);
 
-	/* Return the status from the upstream callback. */
+	/* Make sure the length is reasonable. */
+	if (P->len > SIZE_MAX - 20)
+		goto failed;
+
+	/* Do we have the complete packet? */
+	if (datalen < P->len + 20)
+		goto nopacket;
+
+	/* Verify the data checksum. */
+	CRC32C_Init(&ctx);
+	CRC32C_Update(&ctx, &data[16], P->len);
+	CRC32C_Final(cbuf, &ctx);
+	for (i = 0; i < 4; i++)
+		cbuf[i] ^= data[16 + P->len + i];
+	if (memcmp(&data[12], cbuf, 4)) {
+		warn0("Incorrect CRC on packet data");
+		goto failed;
+	}
+
+	/* Point at the data. */
+	P->buf = &data[16];
+
+nopacket:
+	/* Success! */
+	return (0);
+
+failed:
+	/* Failure! */
+	return (-1);
+}
+
+/**
+ * wire_readpacket_wait(R, callback, cookie):
+ * Wait until a packet is available to be read from ${R} or a failure occurs
+ * while reading (e.g., EOF); then invoke ${callback}(${cookie}, status) where
+ * status is 0 on success or 1 on error.  Return a cookie which can be passed
+ * to wire_readpacket_wait_cancel.
+ */
+void *
+wire_readpacket_wait(struct netbuf_read * R,
+    int (* callback)(void *, int), void * cookie)
+{
+	struct wait_cookie * W;
+
+	/* Bake a cookie. */
+	if ((W = mpool_wait_cookie_malloc()) == NULL)
+		goto err0;
+	W->R = R;
+	W->callback = callback;
+	W->cookie = cookie;
+
+	/* Wait for a header to be available. */
+	if (netbuf_read_wait(W->R, 16, callback_wait_gotheader, W))
+		goto err0;
+
+	/* Success! */
+	return (W);
+
+err0:
+	/* Failure! */
+	return (NULL);
+}
+
+/* A header is available; parse it and wait for the entire packet. */
+static int
+callback_wait_gotheader(void * cookie, int status)
+{
+	CRC32C_CTX ctx;
+	struct wait_cookie * W = cookie;
+	uint8_t * data;
+	size_t datalen;
+	size_t len;
+	uint8_t cbuf[4];
+	int rc;
+
+	/* Did we fail? */
+	if (status)
+		goto failed;
+
+	/* Grab the header and verify the checksum. */
+	netbuf_read_peek(W->R, &data, &datalen);
+	CRC32C_Init(&ctx);
+	CRC32C_Update(&ctx, data, 12);
+	CRC32C_Final(cbuf, &ctx);
+	if (memcmp(&data[12], cbuf, 4)) {
+		warn0("Incorrect CRC on packet header");
+		goto failed;
+	}
+
+	/* Parse length and wait for the complete packet to be available. */
+	len = be32dec(&data[8]);
+	if (len > SIZE_MAX - 20)
+		goto failed;
+	if (netbuf_read_wait(W->R, len + 20, callback_wait_gotdata, W))
+		goto failed;
+
+	/* Success! */
+	return (0);
+
+failed:
+	/* Perform a failure callback. */
+	rc = (W->callback)(W->cookie, 1);
+
+	/* Free the cookie. */
+	mpool_wait_cookie_free(W);
+
+	/* Return status from callback. */
 	return (rc);
+}
+
+/* A packet is available; invoke the upstream callback. */
+static int
+callback_wait_gotdata(void * cookie, int status)
+{
+	struct wait_cookie * W = cookie;
+	int rc;
+
+	/* Invoke the upstream callback. */
+	rc = (W->callback)(W->cookie, status);
+
+	/* Free the cookie. */
+	mpool_wait_cookie_free(W);
+
+	/* Return status from callback. */
+	return (rc);
+}
+
+/**
+ * wire_readpacket_wait_cancel(cookie):
+ * Cancel the packet wait for which ${cookie} was returned.  Do not invoke
+ * the packet wait callback.
+ */
+void
+wire_readpacket_wait_cancel(void * cookie)
+{
+	struct wait_cookie * W = cookie;
+
+	/* Cancel the buffered reader wait. */
+	netbuf_read_wait_cancel(W->R);
+
+	/* Free the cookie. */
+	mpool_wait_cookie_free(W);
+}
+
+/**
+ * wire_readpacket_consume(R, P):
+ * Consume from the reader ${R} the packet ${P}, which it must have returned
+ * via wire_readpacket_peek.
+ */
+void
+wire_readpacket_consume(struct netbuf_read * R, struct wire_packet * P)
+{
+
+	/* Consume the packet. */
+	netbuf_read_consume(R, P->len + 20);
 }
 
 /**
@@ -59,22 +241,22 @@ void *
 wire_readpacket(struct netbuf_read * R,
     int (* callback)(void *, struct wire_packet *), void * cookie)
 {
-	struct readpacket * RP;
+	struct read_cookie * RP;
 
-	/* Allocate a structure and record parameters. */
-	if ((RP = mpool_readpacket_malloc()) == NULL)
+	/* Bake a cookie. */
+	if ((RP = mpool_read_cookie_malloc()) == NULL)
 		goto err0;
 	RP->callback = callback;
 	RP->cookie = cookie;
 	RP->R = R;
 
-	/* Allocate a packet structure with no packet buffer yet. */
+	/* Allocate a packet structure. */
 	if ((RP->packet = wire_packet_malloc()) == NULL)
 		goto err1;
-	RP->packet->buf = NULL;
 
-	/* Attempt to read the request ID and record length. */
-	if (netbuf_read_read(RP->R, RP->hbuf, 16, readheader, RP))
+	/* Wait for a packet to be ready. */
+	if ((RP->wait_cookie =
+	    wire_readpacket_wait(RP->R, callback_read_gotpacket, RP)) == NULL)
 		goto err2;
 
 	/* Success! */
@@ -83,88 +265,57 @@ wire_readpacket(struct netbuf_read * R,
 err2:
 	wire_packet_free(RP->packet);
 err1:
-	mpool_readpacket_free(RP);
+	mpool_read_cookie_free(RP);
 err0:
 	/* Failure! */
 	return (NULL);
 }
 
-/* We've got the header.  Validate and read the rest of the packet. */
+/* A packet is ready.  Read it and pass it to the callback. */
 static int
-readheader(void * cookie, int status)
+callback_read_gotpacket(void * cookie, int status)
 {
-	struct readpacket * RP = cookie;
-	CRC32C_CTX ctx;
-	uint8_t cbuf[4];
+	struct read_cookie * RP = cookie;
+	uint8_t * buf;
+	int rc;
 
-	/* If status is non-zero, we failed. */
+	/* Did we fail? */
 	if (status)
 		goto failed;
 
-	/* Check the CRC. */
-	CRC32C_Init(&ctx);
-	CRC32C_Update(&ctx, RP->hbuf, 12);
-	CRC32C_Final(cbuf, &ctx);
-	if (memcmp(&RP->hbuf[12], cbuf, 4)) {
-		warn0("Incorrect CRC on header");
-		goto failed;
-	}
-
-	/* Copy ID and record length. */
-	RP->packet->ID = be64dec(&RP->hbuf[0]);
-	RP->packet->len = be32dec(&RP->hbuf[8]);
-
-	/* Allocate record buffer, plus an extra 4 bytes to hold CRC. */
-	if (RP->packet->len > SIZE_MAX - 4) {
-		errno = ENOMEM;
-		goto failed;
-	}
-	if ((RP->packet->buf = malloc(RP->packet->len + 4)) == NULL)
+	/* Try to read a packet. */
+	if (wire_readpacket_peek(RP->R, RP->packet))
 		goto failed;
 
-	/* Read the record plus CRC. */
-	if (netbuf_read_read(RP->R, RP->packet->buf,
-	    RP->packet->len + 4, readrec, RP))
-		goto failed;
+	/* Sanity-check: We should have a packet. */
+	assert(RP->packet->buf != NULL);
 
-	/* Success! */
-	return (0);
+	/* Copy the packet buffer: Our callback will own it. */
+	if ((buf = malloc(RP->packet->len)) == NULL)
+		goto failed;
+	memcpy(buf, RP->packet->buf, RP->packet->len);
+	RP->packet->buf = buf;
+
+	/* Consume the packet. */
+	wire_readpacket_consume(RP->R, RP->packet);
+
+	/* We're ready to invoke the callback. */
+	goto docallback;
 
 failed:
-	/* Perform the callback. */
-	return (docallback(RP, NULL));
-}
+	/* Free the embryonic packet; we won't pass it to the callback. */
+	wire_packet_free(RP->packet);
+	RP->packet = NULL;
 
-/* We've got the entire packet.  Validate and pass upstream. */
-static int
-readrec(void * cookie, int status)
-{
-	struct readpacket * RP = cookie;
-	CRC32C_CTX ctx;
-	uint8_t cbuf[4];
-	size_t i;
+docallback:
+	/* Invoke the callback. */
+	rc = (RP->callback)(RP->cookie, RP->packet);
 
-	/* If status is non-zero, we failed. */
-	if (status)
-		goto failed;
+	/* Free the cookie. */
+	mpool_read_cookie_free(RP);
 
-	/* Check the CRC. */
-	CRC32C_Init(&ctx);
-	CRC32C_Update(&ctx, RP->packet->buf, RP->packet->len);
-	CRC32C_Final(cbuf, &ctx);
-	for (i = 0; i < 4; i++)
-		cbuf[i] ^= RP->packet->buf[RP->packet->len + i];
-	if (memcmp(&RP->hbuf[12], cbuf, 4)) {
-		warn0("Incorrect CRC on data");
-		goto failed;
-	}
-
-	/* Perform the callback. */
-	return (docallback(RP, RP->packet));
-
-failed:
-	/* Perform the callback. */
-	return (docallback(RP, NULL));
+	/* Return status from callback. */
+	return (rc);
 }
 
 /**
@@ -175,13 +326,14 @@ failed:
 void
 wire_readpacket_cancel(void * cookie)
 {
-	struct readpacket * RP = cookie;
+	struct read_cookie * RP = cookie;
 
-	/* Cancel the read. */
-	netbuf_read_cancel(RP->R);
+	/* Stop waiting for a packet to arrive. */
+	wire_readpacket_wait_cancel(RP->wait_cookie);
 
-	/* Free the packet and cookie. */
-	free(RP->packet->buf);
+	/* Free the embryonic packet. */
 	wire_packet_free(RP->packet);
-	mpool_readpacket_free(RP);
+
+	/* Free the cookie. */
+	mpool_read_cookie_free(RP);
 }

cc1d435aae28ef85b8f563b635cb7ae50d421c34
Colin Percival <cperciva@tarsnap.com>
2011-11-24 14:48:24 +0000

    Add HTTP request code and a very crude test (3 GETs dumped to stdout, with no checking that results are correct).

---
+++ lib/http/http.h
@@ -0,0 +1,58 @@
+#ifndef _HTTP_H_
+#define _HTTP_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+/* Opaque type. */
+struct sock_addr;
+
+struct http_header {
+	const char * header;
+	const char * value;
+};
+
+struct http_request {
+	const char * method;
+	const char * path;
+	size_t nheaders;
+	struct http_header * headers;
+	size_t bodylen;
+	const uint8_t * body;
+};
+
+struct http_response {
+	int status;
+	size_t nheaders;
+	struct http_header * headers;
+	size_t bodylen;
+	uint8_t * body;
+};
+
+/**
+ * http_request(addrs, request, maxrlen, callback, cookie):
+ * Open a connection to ${addrs} and send the HTTP request ${request}.  Read a
+ * response with a body of up to ${maxrlen} bytes and invoke the provided
+ * callback as ${callback}(${cookie}, ${response}), with response == NULL if
+ * no response was read (e.g., on connection error).  Return a cookie which can
+ * be passed to http_request_cancel.
+ *
+ * If the response has no body, the response structure will have bodylen == 0
+ * and body == NULL; if there is a body larger than ${maxrlen} bytes, the
+ * response structure will have bodylen == (size_t)(-1) and body == NULL.
+ * The callback is responsible for freeing the response body buffer (if any),
+ * but not the rest of the response; it must copy any header strings before it
+ * returns.  The provided request body buffer (if any) must remain valid until
+ * the callback is invoked.
+ */
+void * http_request(struct sock_addr * const *, struct http_request *, size_t,
+    int (*)(void *, struct http_response *), void *);
+
+/**
+ * http_request_cancel(cookie):
+ * Cancel the HTTP request for which ${cookie} was returned by http_request.
+ * Do not invoke the associated callback function.
+ */
+void http_request_cancel(void *);
+
+#endif /* !_HTTP_H_ */

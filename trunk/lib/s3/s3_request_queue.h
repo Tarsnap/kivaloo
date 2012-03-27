aeb4aa41b77c1f5087e9d30de8df2b78f1c96122
cperciva <cperciva@158ec33e-9530-43ce-08a2-6ca7ccd1c3f5>
2012-03-27 09:52:12 +0000

    Add an S3 request queue: You initialize it with keys and a maximum number of simultaneous connections; you feed it endpoint addresses and requests; and it launches them and reports back when they are done.

---
+++ trunk/lib/s3/s3_request_queue.h
@@ -0,0 +1,55 @@
+#ifndef _S3_REQUEST_QUEUE_H_
+#define _S3_REQUEST_QUEUE_H_
+
+/* Opaque types. */
+struct http_response;
+struct sock_addr;
+struct s3_request;
+struct s3_request_queue;
+
+/**
+ * s3_request_queue_init(key_id, key_secret, conns):
+ * Create an S3 request queue using the AWS Key ID ${key_id} and the Secret
+ * Access Key ${key_secret} to perform up to ${conns} simultaneous requests.
+ */
+struct s3_request_queue * s3_request_queue_init(const char *, const char *,
+    size_t);
+
+/**
+ * s3_request_queue_addaddr(Q, addr, ttl):
+ * Add the address ${addr} to the S3 request queue ${Q}, valid for the next
+ * ${ttl} seconds.  The address ${addr} is copied and does not need to remain
+ * valid after the call returns.
+ */
+int s3_request_queue_addaddr(struct s3_request_queue *,
+    const struct sock_addr *, int);
+
+/**
+ * s3_request_queue(Q, request, maxrlen, callback, cookie):
+ * Using the S3 request queue ${Q}, queue the S3 request ${request} to be
+ * performed using a target address selected from those provided via the
+ * s3_request_queue_addaddr function and the AWS Key ID and Secret Access Key
+ * provided via the s3_request_queue_init function.  Requests which fail due
+ * to the HTTP connection breaking or with HTTP 500 or 503 responses are
+ * retried.  The S3 request structure ${request} must remain valid until the
+ * callback is performed or the request queue is freed.  Behave identically to
+ * http_request otherwise.
+ */
+int s3_request_queue(struct s3_request_queue *, struct s3_request *, size_t,
+    int (*)(void *, struct http_response *), void *);
+
+/**
+ * s3_request_queue_flush(Q):
+ * Flush the S3 request queue ${Q}.  Any queued requests will be dropped; no
+ * callbacks will be performed.
+ */
+void s3_request_queue_flush(struct s3_request_queue *);
+
+/**
+ * s3_request_queue_free(Q):
+ * Free the S3 request queue ${Q}.  Any queued requests will be dropped; no
+ * callbacks will be performed.
+ */
+void s3_request_queue_free(struct s3_request_queue *);
+
+#endif /* !_S3_REQUEST_QUEUE_H_ */

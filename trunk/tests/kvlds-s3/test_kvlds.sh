bd4240bc2c275cfbea09bca1d16566c562d3a654
cperciva <cperciva@158ec33e-9530-43ce-08a2-6ca7ccd1c3f5>
2012-03-27 10:02:48 +0000

    Add test script for the kvlds/lbs-s3/s3 stack.

---
+++ trunk/tests/kvlds-s3/test_kvlds.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+# Paths
+S3=../../s3/s3
+LBS=../../lbs-s3/lbs-s3
+KVLDS=../../kvlds/kvlds
+TESTKVLDS=./test_kvlds
+TMPDIR=`pwd`/tmp
+SOCKS3=$TMPDIR/sock_s3
+SOCKL=$TMPDIR/sock_lbs
+SOCKK=$TMPDIR/sock_kvlds
+REGION=s3-us-west-2
+BUCKET=kivaloo-test
+
+# Clean up any old tests
+rm -rf $TMPDIR
+
+# Start S3 daemon
+mkdir $TMPDIR
+chflags nodump $TMPDIR
+$S3 -s $SOCKS3 -r $REGION -k ~/.s3/aws.key
+
+# Start LBS
+$LBS -s $SOCKL -t $SOCKS3 -b 512 -B $BUCKET
+
+# Start KVLDS (the small number of pages should trigger evictions)
+$KVLDS -s $SOCKK -l $SOCKL -v 104 -C 1024
+
+# Test basic operations
+echo -n "Testing KVLDS operations against LBS-S3... "
+if $TESTKVLDS $SOCKK; then
+	echo " PASSED!"
+else
+	echo " FAILED!"
+	exit 1
+fi
+
+# Test shutting down and restarting KVLDS
+echo -n "Testing disconnect and reconnect to LBS-S3... "
+kill `cat $SOCKK.pid`
+rm $SOCKK.pid $SOCKK
+$KVLDS -s $SOCKK -l $SOCKL -v 104 -C 1024
+if $TESTKVLDS $SOCKK; then
+	echo " PASSED!"
+else
+	echo " FAILED!"
+	exit 1
+fi
+kill `cat $SOCKK.pid`
+rm $SOCKK.pid $SOCKK
+
+# Check that killing KVLDS can't break it
+echo -n "Testing LBS-S3 tolerance of KVLDS crashes..."
+$KVLDS -s $SOCKK -l $SOCKL -v 104
+$TESTKVLDS $SOCKK 2>/dev/null &
+sleep 0.1 && kill `cat $SOCKK.pid`
+rm $SOCKK.pid $SOCKK
+$KVLDS -s $SOCKK -l $SOCKL -v 104
+$TESTKVLDS $SOCKK 2>/dev/null &
+sleep 0.1 && kill `cat $SOCKK.pid`
+rm $SOCKK.pid $SOCKK
+$KVLDS -s $SOCKK -l $SOCKL -v 104
+$TESTKVLDS $SOCKK 2>/dev/null &
+sleep 0.1 && kill `cat $SOCKK.pid`
+rm $SOCKK.pid $SOCKK
+$KVLDS -s $SOCKK -l $SOCKL -v 104
+if $TESTKVLDS $SOCKK; then
+	echo " PASSED!"
+else
+	echo " FAILED!"
+	exit 1
+fi
+
+# Shut down daemons and and clean up
+kill `cat $SOCKK.pid`
+rm $SOCKK.pid $SOCKK
+kill `cat $SOCKL.pid`
+rm $SOCKL.pid $SOCKL
+kill `cat $SOCKS3.pid`
+rm $SOCKS3.pid $SOCKS3
+rm -r $TMPDIR
+
+# Make sure LBS-S3 doesn't leak memory
+echo -n "Checking for memory leaks in LBS-S3..."
+mkdir $TMPDIR
+chflags nodump $TMPDIR
+$S3 -s $SOCKS3 -r $REGION -k ~/.s3/aws.key -1
+ktrace -i -f ktrace-lbs-s3.out env MALLOC_OPTIONS=JUV		\
+    $LBS -s $SOCKL -t $SOCKS3 -b 512 -B $BUCKET -1
+$KVLDS -s $SOCKK -l $SOCKL -v 104 -C 1024 -1
+$TESTKVLDS $SOCKK
+sleep 1
+rm $SOCKK.pid $SOCKK
+rm $SOCKL.pid $SOCKL
+rm $SOCKS3.pid $SOCKS3
+rm -r $TMPDIR
+
+# Process ktrace-lbs output
+kdump -Ts -f ktrace-lbs-s3.out |			\
+    grep ' lbs-s3 ' > kdump-lbs-s3.out
+sh ../tools/memleak/memleak.sh kdump-lbs-s3.out lbs-s3.leak 2>leak.tmp
+if grep -q 'leaked 0 bytes' leak.tmp; then
+	echo " PASSED!"
+	rm ktrace-lbs-s3.out kdump-lbs-s3.out
+	rm lbs-s3.leak
+else
+	cat leak.tmp | tr -d '\n'
+	echo && echo "  -> memory leaks shown in lbs-s3.leak"
+fi
+rm leak.tmp

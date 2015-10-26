946e06ac413b341801622d92fafffeef670f2c1c
Colin Percival <cperciva@tarsnap.com>
2015-10-26 01:04:41 -0700

    Switch from fgetln to getline in tests/heap
    The BSD fgetln function is unportable, and using fgets would impose a
    line length limit -- not that it matters for the current test case, but
    I hate having arbitrary limits even in test code.
    
    Fortunately POSIX.1-2008 now provides getline, which has similar
    functionality to fgetln and supports arbitrary line lengths.
    
    Minor build fallout: Hook warnp into the tests/heap build so that I can
    use it; and add add -DPOSIX_C_SOURCE=200809L to tell FreeBSD to expose
    the getline function.

--- tests/heap/main.c
+++ tests/heap/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "ptrheap.h"
+#include "warnp.h"
 
 static int
 compar(void * cookie, const void * x, const void * y)
@@ -20,7 +21,10 @@ main(int argc, char * argv[])
 {
 	struct ptrheap * H;
 	char * s, * dups;
-	size_t l;
+	size_t buflen;
+	ssize_t l;
+
+	WARNP_INIT;
 
 	(void)argc; /* UNUSED */
 	(void)argv; /* UNUSED */
@@ -30,9 +34,19 @@ main(int argc, char * argv[])
 		exit(1);
 
 	/* Suck in the input. */
-	while ((s = fgetln(stdin, &l)) != NULL) {
-		/* NUL-terminate. */
-		s[l-1] = '\0';
+	s = NULL;
+	buflen = 0;
+	while ((l = getline(&s, &buflen, stdin)) != -1) {
+		/* Check for embedded NUL characters. */
+		if (strlen(s) != (size_t)l) {
+			warnp("Line of length %zu has embedded NUL: %s",
+			    (size_t)l, s);
+			exit(1);
+		}
+
+		/* Remove trailing '\n'. */
+		while ((l > 0) && (s[l - 1] == '\n'))
+			s[--l] = '\0';
 
 		/* Duplicate string. */
 		if ((dups = strdup(s)) == NULL)
@@ -41,7 +55,10 @@ main(int argc, char * argv[])
 		/* Insert string. */
 		if (ptrheap_add(H, dups))
 			exit(1);
-	};
+	}
+
+	/* Free string allocated by getline. */
+	free(s);
 
 	/* Write out the lines in order. */
 	while ((s = ptrheap_getmin(H)) != NULL) {

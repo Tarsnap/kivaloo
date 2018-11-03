eed472a71a9f47b0aa441baab5a90185aed01f0d
Colin Percival <cperciva@tarsnap.com>
2018-11-03 03:17:06 -0700

    Add json_find.
    This searches a JSON object to find the value associated with a
    provided key.  When the goal is to extract a specific value this
    can be far more efficient than parsing an entire object only to
    throw away most of it unused a moment later.

---
+++ libcperciva/util/json.h
@@ -0,0 +1,14 @@
+#ifndef _JSON_H_
+#define _JSON_H_
+
+#include <stdint.h>
+
+/**
+ * json_find(buf, end, s):
+ * If there is a valid JSON object which starts at ${buf} and ends before or
+ * at ${end} and said object contains a name/value pair with name ${s},
+ * return a pointer to the associated value.  Otherwise, return ${end}.
+ */
+const uint8_t * json_find(const uint8_t *, const uint8_t *, const char *);
+
+#endif /* !_JSON_H_ */

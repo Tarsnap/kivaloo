72bfb4f5d74762186e074a2b4cffe550fa139468
Graham Percival <gperciva@tarsnap.com>
2019-05-28 22:16:29 -0700

    lbs: cast blocklen to (off_t) as needed
    The main lbs binary checks that blocklen is at most 2^17.  I didn't
    think that we should hard-code that limit in the storage layer, but it's
    useful to ensure that blocklen will fit into (off_t).

--- lbs/storage.c
+++ lbs/storage.c
@@ -42,6 +42,11 @@ storage_init(const char * storagedir, size_t blocklen, long latency,
 
 	/* Sanity-check the block size. */
 	assert(blocklen > 0);
+#ifdef OFF_MAX
+	assert(blocklen <= OFF_MAX);
+#else
+	assert(blocklen <= INT32_MAX);
+#endif
 
 	/* Allocate structure and fill in static data. */
 	if ((S = malloc(sizeof(struct storage_state))) == NULL)
@@ -100,7 +105,7 @@ storage_init(const char * storagedir, size_t blocklen, long latency,
 		}
 
 		/* Does it have a non-integer number of blocks? */
-		if ((sf->len % S->blocklen) != 0) {
+		if ((sf->len % (off_t)S->blocklen) != 0) {
 			/* Not permitted for files in the middle. */
 			if (elasticqueue_getlen(files) > 1) {
 				warn0("Block storage file has non-integer"
@@ -116,13 +121,14 @@ storage_init(const char * storagedir, size_t blocklen, long latency,
 			 */
 			if ((s = storage_util_mkpath(S, sf->fileno)) == NULL)
 				goto err3;
-			if (truncate(s, sf->len - (sf->len % S->blocklen)))
+			if (truncate(s, sf->len - (sf->len %
+			    (off_t)S->blocklen)))
 				goto err4;
 			free(s);
 		}
 
 		/* Compute number of blocks. */
-		fs.len = sf->len / S->blocklen;
+		fs.len = sf->len / (off_t)S->blocklen;
 
 		/* Add to the queue of block file state structures. */
 		if (elasticqueue_add(S->files, &fs))

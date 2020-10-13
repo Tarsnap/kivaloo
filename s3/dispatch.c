Tarsnap/kivaloo

commit 16b056c0db14d97f22b93baecbaea55ad10e778c

s3: explicit cast (uint32_t)bodylen
Also added a sanity check.

This should always be true, because:
- the maximum size of the body length is at most PROTO_S3_MAXLEN
  (which is 0x80000000), as checked in lib/proto_s3/proto_s3_client.c
  for both the _get() and _put() functions.
- the HTTP request layer checks that the response won't be larger than
  res->bodylen_max (which has been set to the value specified in the
  proto_s3 call).

s3/dispatch.c
@@ -268,14 +268,21 @@ callback_response(void * cookie, struct http_response * res)
 		if ((res->body == NULL) || (s3_verifyetag(res) == 0))
 			res->status = 0;
 
+		/* Sanity check. */
+		assert(res->bodylen <= UINT32_MAX);
+
 		/* Send the response. */
 		if (proto_s3_response_get(D->writeq, R->R.ID, res->status,
-		    res->bodylen, res->body))
+		    (uint32_t)res->bodylen, res->body))
 			goto err1;
 		break;
 	case PROTO_S3_RANGE:
+		/* Sanity check. */
+		assert(res->bodylen <= UINT32_MAX);
+
+		/* Send the response. */
 		if (proto_s3_response_range(D->writeq, R->R.ID, res->status,
-		    res->bodylen, res->body))
+		    (uint32_t)res->bodylen, res->body))
 			goto err1;
 		break;
 	case PROTO_S3_HEAD:

commit dde7c8b9a446665675057219c2aabbd886b10c39

s3: allow bodylen=(-1) in sanity check
I broke this in 16b056c0db14d97f22b93baecbaea55ad10e778c; the s3 tests
explicitly check that it can handle "failure" cases, and not allowing an
HTTP body length of -1 broke that.

I didn't notice the error because I didn't have s3 keys set up, so the
test suite didn't run tests/s3.  The same applies to the Travis-CI
tests: we don't have s3 keys there, so it doesn't check s3.

s3/dispatch.c
@@ -269,7 +269,7 @@ callback_response(void * cookie, struct http_response * res)
 			res->status = 0;
 
 		/* Sanity check. */
-		assert(res->bodylen <= UINT32_MAX);
+		assert((res->bodylen == -1) || (res->bodylen <= UINT32_MAX));
 
 		/* Send the response. */
 		if (proto_s3_response_get(D->writeq, R->R.ID, res->status,
@@ -278,7 +278,7 @@ callback_response(void * cookie, struct http_response * res)
 		break;
 	case PROTO_S3_RANGE:
 		/* Sanity check. */
-		assert(res->bodylen <= UINT32_MAX);
+		assert((res->bodylen == -1) || (res->bodylen <= UINT32_MAX));
 
 		/* Send the response. */
 		if (proto_s3_response_range(D->writeq, R->R.ID, res->status,
Tarsnap/kivaloo

commit dde7c8b9a446665675057219c2aabbd886b10c39

s3: allow bodylen=(-1) in sanity check
I broke this in 16b056c0db14d97f22b93baecbaea55ad10e778c; the s3 tests
explicitly check that it can handle "failure" cases, and not allowing an
HTTP body length of -1 broke that.

I didn't notice the error because I didn't have s3 keys set up, so the
test suite didn't run tests/s3.  The same applies to the Travis-CI
tests: we don't have s3 keys there, so it doesn't check s3.

s3/dispatch.c
@@ -269,7 +269,7 @@ callback_response(void * cookie, struct http_response * res)
 			res->status = 0;
 
 		/* Sanity check. */
-		assert(res->bodylen <= UINT32_MAX);
+		assert((res->bodylen == -1) || (res->bodylen <= UINT32_MAX));
 
 		/* Send the response. */
 		if (proto_s3_response_get(D->writeq, R->R.ID, res->status,
@@ -278,7 +278,7 @@ callback_response(void * cookie, struct http_response * res)
 		break;
 	case PROTO_S3_RANGE:
 		/* Sanity check. */
-		assert(res->bodylen <= UINT32_MAX);
+		assert((res->bodylen == -1) || (res->bodylen <= UINT32_MAX));
 
 		/* Send the response. */
 		if (proto_s3_response_range(D->writeq, R->R.ID, res->status,

commit d21e682dbedbde4f02472f13e29e19dc015362ba

s3: explicit cast to (size_t)
I broke this in dde7c8b9a446665675057219c2aabbd886b10c39, which itself
was a fix for when I broke the s3 test in
16b056c0db14d97f22b93baecbaea55ad10e778c.

s3/dispatch.c
@@ -269,7 +269,8 @@ callback_response(void * cookie, struct http_response * res)
 			res->status = 0;
 
 		/* Sanity check. */
-		assert((res->bodylen == -1) || (res->bodylen <= UINT32_MAX));
+		assert((res->bodylen == (size_t)-1) ||
+		    (res->bodylen <= UINT32_MAX));
 
 		/* Send the response. */
 		if (proto_s3_response_get(D->writeq, R->R.ID, res->status,
@@ -278,7 +279,8 @@ callback_response(void * cookie, struct http_response * res)
 		break;
 	case PROTO_S3_RANGE:
 		/* Sanity check. */
-		assert((res->bodylen == -1) || (res->bodylen <= UINT32_MAX));
+		assert((res->bodylen == (size_t)-1) ||
+		    (res->bodylen <= UINT32_MAX));
 
 		/* Send the response. */
 		if (proto_s3_response_range(D->writeq, R->R.ID, res->status,
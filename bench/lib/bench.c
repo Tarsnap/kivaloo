3c677bbb8bfbbfbddc5b4fdb6d0d3099d4317f36
Graham Percival <gperciva@tarsnap.com>
2020-10-18 12:04:37 -0700

    bench: record data in seconds
    This also adds bench_get_ticks(), which is only intended for use in
    debugging (e.g., deciding how many seconds to wait before recording
    data).

--- bench/lib/bench.c
+++ bench/lib/bench.c
@@ -1,8 +1,10 @@
 #include <sys/time.h>
 
 #include <assert.h>
+#include <inttypes.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "monoclock.h"
 #include "warnp.h"
@@ -14,7 +16,8 @@ struct bench {
 	size_t num_seconds;
 
 	/* Benchmark data. */
-	uint64_t ticks;
+	uint64_t * ticks;
+	size_t tick_pos;
 
 	/* Internal time-keeping. */
 	struct timeval tv_start;
@@ -41,24 +44,31 @@ bench_init(size_t start, size_t num_seconds)
 		goto err0;
 	}
 	B->num_seconds = num_seconds;
-	B->ticks = 0;
 	B->started = 0;
+
+	/* Allocate and initialize tick counts. */
+	if ((B->ticks = malloc(num_seconds * sizeof(uint64_t))) == NULL) {
+		warn0("malloc");
+		goto err1;
+	}
+	memset(B->ticks, 0, num_seconds * sizeof(uint64_t));
+	B->tick_pos = 0;
 
 	/* Get current time. */
 	if (monoclock_get(&tv_now)) {
 		warnp("monoclock_get");
-		goto err1;
+		goto err2;
 	}
 
-	/* Prepare benchmark time values. */
+	/* Prepare start benchmark time value. */
 	B->tv_start.tv_sec = tv_now.tv_sec + (int)start;
 	B->tv_start.tv_usec = tv_now.tv_usec;
-	B->tv_end.tv_sec = tv_now.tv_sec + (int)(start + num_seconds);
-	B->tv_end.tv_usec = tv_now.tv_usec;
 
 	/* Success! */
 	return (B);
 
+err2:
+	free(B->ticks);
 err1:
 	free(B);
 err0:
@@ -106,16 +116,30 @@ bench_tick(struct bench * B, int * done)
 
 		/* Start recording ticks. */
 		B->started = 1;
+
+		/* Start tracking seconds. */
+		B->tv_end.tv_sec = tv_now.tv_sec + 1;
+		B->tv_end.tv_usec = tv_now.tv_usec;
 	}
 
-	/* Have we finished? */
+	/* Have we finished a second? */
 	if (tv_cmp(tv_now, B->tv_end) > 0) {
-		*done = 1;
-		goto done;
+		/* Have we run out of seconds to record? */
+		if (B->tick_pos + 1 >= B->num_seconds) {
+			*done = 1;
+			goto done;
+		}
+
+		/* Prepare to record in a new position. */
+		B->tick_pos++;
+
+		/* Set up the next second. */
+		B->tv_end.tv_sec = tv_now.tv_sec + 1;
+		B->tv_end.tv_usec = tv_now.tv_usec;
 	}
 
-	/* Record value. */
-	B->ticks++;
+	/* Record value in the current position. */
+	B->ticks[B->tick_pos] += 1;
 
 done:
 	/* Success! */
@@ -127,15 +151,33 @@ err0:
 }
 
 /**
+ * bench_get_ticks(B):
+ * Get the array containing the number of ticks per second.  The callee is
+ * responsible for knowing the length of the array.
+ */
+uint64_t *
+bench_get_ticks(struct bench * B)
+{
+
+	return (B->ticks);
+}
+
+/**
  * bench_mean(B):
  * Return the mean number of ticks per second during the benchmark period.
  */
 uint64_t
 bench_mean(struct bench * B)
 {
+	size_t i;
+	uint64_t sum = 0;
+
+	/* Calculate sum. */
+	for (i = 0; i < B->num_seconds; i++)
+		sum += B->ticks[i];
 
 	/* Return the mean. */
-	return (B->ticks / B->num_seconds);
+	return (sum / B->num_seconds);
 }
 
 /**
@@ -150,5 +192,6 @@ bench_free(struct bench * B)
 	if (B == NULL)
 		return;
 
+	free(B->ticks);
 	free(B);
 }
